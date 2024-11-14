e3665432fee18d708a05cd8a447ba0823fc57187
kpthiel <64731939+kpthiel@users.noreply.github.com>
2024-11-14 18:21:18 +0100

    Update Charge-Control_Ver_1.5.11.js

--- Charge-Control_Ver_1.5.11.js
+++ Charge-Control_Ver_1.5.11.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 //------------------------------------------------------------------------------------------------------
 //++++++++++++++++++++++++++++++++++++++++++  USER ANPASSUNGEN +++++++++++++++++++++++++++++++++++++++++
 const LogparserSyntax = true                                                                            // Wenn true wird die LOG Ausgabe an Adapter Logparser angepasst
